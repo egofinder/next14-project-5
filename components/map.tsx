f844d783618d6656a8649ff958053279a5b192ba
Benjamin Jeon <baekhakjeon@gmail.com>
2024-02-14 17:45:45 -0800

    지도 컴포넌트를 Mapbox로 변경

--- components/map.tsx
+++ components/map.tsx
@@ -1,20 +1,7 @@
 "use client";
 
-import L from "leaflet";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
-
-import "leaflet/dist/leaflet.css";
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
-
-// @ts-ignore
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerIcon2x.src,
-  iconUrl: markerIcon.src,
-  shadowUrl: markerShadow.src,
-});
+import Mapbox from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 interface MapProps {
   center?: number[];
@@ -22,19 +9,30 @@ interface MapProps {
 
 const Map = ({ center }: MapProps) => {
   return (
-    <MapContainer
-      center={(center as L.LatLngExpression) || [33.977666, -117.909685]}
-      zoom={center ? 4 : 2}
-      scrollWheelZoom={false}
-      className="h-[35vh] rounded-lg"
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">
-            OpenStreetMap</a> contributors'
-      />
-      {center && <Marker position={center as L.LatLngExpression} />}
-    </MapContainer>
+    <Mapbox
+      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+      initialViewState={{
+        longitude: center ? center[1] : -117.909685,
+        latitude: center ? center[0] : 33.977666,
+        zoom: center ? 6 : 2,
+      }}
+      style={{ height: 400 }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      scrollZoom={false}
+    />
+    // <MapContainer
+    //   center={(center as L.LatLngExpression) || [33.977666, -117.909685]}
+    //   zoom={center ? 4 : 2}
+    //   scrollWheelZoom={false}
+    //   className="h-[35vh] rounded-lg"
+    // >
+    //   <TileLayer
+    //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">
+    //         OpenStreetMap</a> contributors'
+    //   />
+    //   {center && <Marker position={center as L.LatLngExpression} />}
+    // </MapContainer>
   );
 };
 
